203c362e8f8f2803ff78b3e5ae9f4115fbd9dd4f
nguyen xuan nhat <xuannhatvn211@gmail.com>
2025-03-04 13:26:19 +0700

    java03

--- src/BT09.java
+++ src/BT09.java
@@ -2,17 +2,120 @@ import java.util.Scanner;
 
 public class BT09 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số (100-999): ");
-        int n = scanner.nextInt();
+        Scanner scanner = new Scanner (System.in);
 
-        if (n < 100 || n > 999) {
-            System.out.println("Số không hợp lệ");
+        System.out.print("Nhập vào một số (100-999): ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number < 100 || number > 999) {
+            System.out.println("Số nhập vào không hợp lệ.");
         } else {
-            String[] words = {"", "Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín"};
-            System.out.println(words[n / 100] + " trăm " + (n % 100 == 0 ? "" : (n % 100 < 10 ? "lẻ " : "") + (n % 100 < 20 ? "mười " : words[(n / 10) % 10] + " mươi ") + (n % 10 == 0 ? "" : (n % 10 == 1 && n / 10 > 1 ? "mốt" : (n % 10 == 5 && n / 10 > 1 ? "lăm" : words[n % 10])))));
+            int hundreds = number / 100;
+            int tens = (number / 10) % 10;
+            int units = number % 10;
+
+            String result = "";
+
+            switch (hundreds) {
+                case 1:
+                    result += "Một trăm ";
+                    break;
+                case 2:
+                    result += "Hai trăm ";
+                    break;
+                case 3:
+                    result += "Ba trăm ";
+                    break;
+                case 4:
+                    result += "Bốn trăm ";
+                    break;
+                case 5:
+                    result += "Năm trăm ";
+                    break;
+                case 6:
+                    result += "Sáu trăm ";
+                    break;
+                case 7:
+                    result += "Bảy trăm ";
+                    break;
+                case 8:
+                    result += "Tám trăm ";
+                    break;
+                case 9:
+                    result += "Chín trăm ";
+                    break;
+            }
+
+            if (tens == 0 && units != 0) {
+                result += "lẻ ";
+            }
+
+            switch (tens) {
+                case 1:
+                    result += "mười ";
+                    break;
+                case 2:
+                    result += "hai mươi ";
+                    break;
+                case 3:
+                    result += "ba mươi ";
+                    break;
+                case 4:
+                    result += "bốn mươi ";
+                    break;
+                case 5:
+                    result += "năm mươi ";
+                    break;
+                case 6:
+                    result += "sáu mươi ";
+                    break;
+                case 7:
+                    result += "bảy mươi ";
+                    break;
+                case 8:
+                    result += "tám mươi ";
+                    break;
+                case 9:
+                    result += "chín mươi ";
+                    break;
+            }
+
+            if (tens != 1) {
+                switch (units) {
+                    case 1:
+                        result += "một";
+                        break;
+                    case 2:
+                        result += "hai";
+                        break;
+                    case 3:
+                        result += "ba";
+                        break;
+                    case 4:
+                        result += "bốn";
+                        break;
+                    case 5:
+                        result += "năm";
+                        break;
+                    case 6:
+                        result += "sáu";
+                        break;
+                    case 7:
+                        result += "bảy";
+                        break;
+                    case 8:
+                        result += "tám";
+                        break;
+                    case 9:
+                        result += "chín";
+                        break;
+                }
+            }
+
+            System.out.println(result.trim());
         }
 
-        scanner.close();
+
     }
 }
--- src/BT10.java
+++ src/BT10.java
@@ -2,44 +2,70 @@ import java.util.Scanner;
 
 public class BT10 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Nhập a, b, c, d: ");
-        double a = scanner.nextDouble(), b = scanner.nextDouble(), c = scanner.nextDouble(), d = scanner.nextDouble();
+        System.out.print("Nhap he so a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Nhap he so b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Nhap he so c: ");
+        double c = sc.nextDouble();
+
+        System.out.print("Nhap he so d: ");
+        double d = sc.nextDouble();
 
         if (a == 0) {
-            solveQuadratic(b, c, d);
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println(d == 0 ? "Phuong trinh co vo so nghiem" : "Phuong trinh vo nghiem");
+                } else {
+                    System.out.println("Nghiem cua phuong trinh la: x = " + (-d / c));
+                }
+            } else {
+                double delta = c * c - 4 * b * d;
+                if (delta > 0) {
+                    double x1 = (-c + Math.sqrt(delta)) / (2 * b);
+                    double x2 = (-c - Math.sqrt(delta)) / (2 * b);
+                    System.out.println("Phuong trinh co hai nghiem phan biet: x1 = " + x1 + ", x2 = " + x2);
+                } else if (delta == 0) {
+                    System.out.println("Phuong trinh co nghiem kep: x = " + (-c / (2 * b)));
+                } else {
+                    System.out.println("Phuong trinh vo nghiem");
+                }
+            }
         } else {
-            solveCubic(a, b, c, d);
-        }
-        scanner.close();
-    }
+            double f = ((3 * c / a) - (b * b / (a * a))) / 3;
+            double g = ((2 * Math.pow(b, 3) / Math.pow(a, 3)) - (9 * b * c / Math.pow(a, 2)) + (27 * d / a)) / 27;
+            double h = (Math.pow(g, 2) / 4) + (Math.pow(f, 3) / 27);
 
-    static void solveQuadratic(double a, double b, double c) {
-        if (a == 0) {
-            System.out.println(b == 0 ? (c == 0 ? "Vô số nghiệm" : "Vô nghiệm") : "Nghiệm: x = " + (-c / b));
-            return;
-        }
-        double delta = b * b - 4 * a * c;
-        if (delta >= 0) {
-            System.out.println("Nghiệm: x1 = " + (-b + Math.sqrt(delta)) / (2 * a) + ", x2 = " + (-b - Math.sqrt(delta)) / (2 * a));
-        } else {
-            System.out.println("Vô nghiệm thực");
-        }
-    }
+            if (h > 0) {
+                double R = -(g / 2) + Math.sqrt(h);
+                double S = Math.cbrt(R);
+                double T = -(g / 2) - Math.sqrt(h);
+                double U = Math.cbrt(T);
+                double x1 = (S + U) - (b / (3 * a));
+                System.out.println("Phuong trinh co mot nghiem thuc duy nhat: x1 = " + x1);
+            } else if (h == 0 && f == 0 && g == 0) {
+                double x1 = -Math.cbrt(d / a);
+                System.out.println("Phuong trinh co mot nghiem thuc duy nhat: x1 = " + x1);
+            } else {
+                double i = Math.sqrt((Math.pow(g, 2) / 4) - h);
+                double j = Math.cbrt(i);
+                double k = Math.acos(-(g / (2 * i)));
+                double L = -j;
+                double M = Math.cos(k / 3);
+                double N = Math.sqrt(3) * Math.sin(k / 3);
+                double P = -(b / (3 * a));
 
-    static void solveCubic(double a, double b, double c, double d) {
-        double delta0 = b * b - 3 * a * c, delta1 = 2 * b * b * b - 9 * a * b * c + 27 * a * a * d;
-        double discriminant = delta1 * delta1 - 4 * delta0 * delta0 * delta0;
-        if (discriminant > 0) {
-            double C = Math.cbrt((delta1 + Math.sqrt(discriminant)) / 2);
-            System.out.println("Nghiệm thực: x = " + (-1 / (3 * a) * (b + C + delta0 / C)));
-        } else {
-            double theta = Math.acos(delta1 / (2 * Math.sqrt(delta0 * delta0 * delta0)));
-            for (int i = 0; i < 3; i++) {
-                double x = -2 * Math.sqrt(delta0) * Math.cos((theta + 2 * Math.PI * i) / 3) - b / (3 * a);
-                System.out.println("Nghiệm: x = " + x);
+                double x1 = 2 * j * Math.cos(k / 3) - (b / (3 * a));
+                double x2 = L * (M + N) + P;
+                double x3 = L * (M - N) + P;
+
+                System.out.println("Phuong trinh co ba nghiem thuc: x1 = " + x1 + ", x2 = " + x2 + ", x3 = " + x3);
             }
         }
+        sc.close();
     }
 }
